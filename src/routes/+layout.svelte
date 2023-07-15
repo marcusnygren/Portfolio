d92c358b17e9cdcc399b6e033c34472bba139403
Marcus Nygren <marcusnygren.mn@gmail.com>
2023-07-15 18:44:30 +0200

    Refactor

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,46 +1,53 @@
-<script>
-	import { LogoLinkedin } from 'carbon-icons-svelte';
-	import '../app.css';
-	import 'carbon-components-svelte/css/all.css';
-	import { Theme, Button } from 'carbon-components-svelte';
-	import WatsonHealthWindowBlackSaturation from 'carbon-icons-svelte/lib/WatsonHealthWindowBlackSaturation.svelte';
+<script lang="ts">
+  import "../app.css";
+  import "carbon-components-svelte/css/g100.css";
+  import {
+    Header,
+    SideNav,
+    SideNavItems,
+    SideNavMenu,
+    SideNavMenuItem,
+    SideNavLink,
+    SideNavDivider,
+    SkipToContent,
+    Content,
+    Grid,
+    Row,
+    Column,
+  } from "carbon-components-svelte";
 
-	import { LocalStorage, Toggle } from 'carbon-components-svelte';
-	import { onMount } from 'svelte';
-	import Menu from './menu.svelte';
-
-	let toggled = false;
-	let events = [];
-
-	onMount(() => {
-		document.documentElement.setAttribute('theme', toggled ? 'g100' : 'g10');
-	});
-
-	let theme = 'g100'; // "g10" | "g100"
-
-	const handleClick = () => {
-		toggled = !toggled;
-		document.documentElement.setAttribute('theme', toggled ? 'g100' : 'g10');
-	};
+  let isSideNavOpen = true;
 </script>
 
-<LocalStorage
-	key="dark-mode"
-	bind:value={toggled}
-	on:save={() => {
-		events = [...events, { event: 'on:save' }];
-	}}
-	on:update={({ detail }) => {
-		events = [...events, { event: 'on:update', detail }];
-	}}
-/>
+<Header company="Developer" platformName="Marcus Nygren" bind:isSideNavOpen>
+  <svelte:fragment slot="skip-to-content">
+    <SkipToContent />
+  </svelte:fragment>
+</Header>
 
-<Theme bind:theme persist persistKey="__carbon-theme" />
+<SideNav bind:isOpen={isSideNavOpen}>
+  <SideNavItems>
+    <SideNavMenuItem href="/" text="About" />
+    <SideNavLink href="career" text="Career" />
+    <SideNavMenu text="Projects">
+      <SideNavMenuItem href="games" text="Games" />
+      <SideNavMenuItem href="web" text="Web" />
+    </SideNavMenu>
+    <SideNavLink href="skills" text="Skills" />
+    <SideNavDivider />
+    <SideNavLink
+      href="https://www.linkedin.com/in/marcusnygren/en-US"
+      text="LinkedIn"
+    />
+  </SideNavItems>
+</SideNav>
 
-<Button on:click={handleClick}
-	><WatsonHealthWindowBlackSaturation size={32} /><span class="pl-2"
-		>Dark mode {toggled ? 'on' : 'off'}</span
-	></Button
->
-
-<Menu><slot /></Menu>
+<Content>
+  <Grid>
+    <Row>
+      <Column>
+        <slot />
+      </Column>
+    </Row>
+  </Grid>
+</Content>
